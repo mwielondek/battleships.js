e2c28afac928fc76925f6f07a4ef1c02fe3768c5
Milosz <miloszw@kth.se>
2015-02-01 23:59:24 +0100

    Started messing around with knockout.js

--- script.js
+++ script.js
@@ -1,9 +1,39 @@
 GRID_SIZE = 9;
 NUM_OF_BOATS = 5;
 
+SHIPS = [
+    {shipType: "Aircraft carrier", shipSize: 5},
+    {shipType: "Battleship", shipSize: 4},
+    {shipType: "Submarine", shipSize: 3},
+    {shipType: "Destroyer", shipSize: 3},
+    {shipType: "Patrol boat", shipSize: 2}
+]
+
+
+function AppViewModel() {
+    this.instructions = ko.observable("Place ships:");
+
+    this.ships = [];
+    for(i = 0; i < SHIPS.length; i++)
+        this.ships.push({ship: SHIPS[i], placed: ko.observable(false)});
+
+}
+my = {viewModel: new AppViewModel()};
+
+ko.bindingHandlers.strikeShip = {
+    update: function(element, valueAccessor) {
+        if (valueAccessor())
+            $(element).addClass("strikeout");
+    }
+}
+
 $(document).ready(function() {
+    $("input#play").prop("disabled", true);
     $("div#container").on("click", "span.cell", buttonClickHandler);
     boats = getRandBoatPos();
+
+    // Activate knockout.js
+    ko.applyBindings(my.viewModel);
 });
 
 var buttonClickHandler = function(event) {
